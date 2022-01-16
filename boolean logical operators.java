251ec157f6da3d3492889ec5bdcd2b32c3b4a842
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-16 17:08:33 -0500

    Add files via upload

---
+++ boolean logical operators.java
@@ -0,0 +1,13 @@
+class Operation
+{
+Public static void main (String []args)
+{
+boolean a = true;
+boolean b = false;
+System.out.println(“a|b” +(a|b));
+System.out.println(“a&b” +(a&b));
+System.out.println(“a^b” +(a^b));
+System.out.println(“(!a&b)|(a&!b)” +((!a&b)|(a&!b)));
+System.out.println(“!a” +(!a));
+}
+}
