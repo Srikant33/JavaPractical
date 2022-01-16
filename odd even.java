f76b6355ebcc3a352b4935f089340b19d89d8b48
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-16 17:09:39 -0500

    Add files via upload

---
+++ odd even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class EvenOdd
+{
+public static void main(String []args)
+{
+Scanner sc = new Scanner (System.in)
+int a;
+a = sc.nestint();
+If(a%2==0)
+ {
+System.out.println(“even number”);
+ }
+else
+{
+System.out.println(“odd number”);
+}
+ }
