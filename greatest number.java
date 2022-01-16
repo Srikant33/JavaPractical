53bcbfff38504d98c83d4555f38d88bd03afc6da
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-16 17:08:50 -0500

    Add files via upload

---
+++ greatest number.java
@@ -0,0 +1,29 @@
+class ClassNesting
+{
+int a , b ;
+void getvalue( int x, int y)
+{
+a=x;
+b=y;
+}
+void getmax()
+{
+rether ((a>b)?a:b);
+}
+void display();
+{
+System.out.println(“greatest”)
+Number is = +(obj.getmax());
+}
+}
+Class Nest
+{
+public static void main (String []args)
+
+{
+class nestingobj = new class nesting();
+obj.getvalue(5,2);
+obj.display();
+}
+}
+
