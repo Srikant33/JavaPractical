15449d65edff51a3deda1baf525d01086026c3ee
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-16 17:10:28 -0500

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,12 @@
+class Conversion
+{
+public static void main (String []args)
+{
+byte b;
+int i = 257;
+double d = 323.142;
+b = (byte)i;
+System.out.println (“values of i after converting it into byte:” +b);
+System.out.println (“value after converting into byte:” (byte)d);
+}
+}
