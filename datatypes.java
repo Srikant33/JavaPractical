e0a41e97469ec49d0c6f0f1a196d829954ec9f90
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-16 17:08:39 -0500

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,33 @@
+Class PrimitiveTypeMinMax{
+public static void main(String[] aargs)
+{
+//int(32-bit signed integer)
+System.out.println(“int(min)=”+integer.MIN_VALUE);
+System.out.println(“int(max)=”+integer.MAX_VALUE);
+System.out.println(“int(bit-length)=”+integerSIZE);
+//Byte(8-bit signed integer)
+System.out.println(“byte(min)=”+integer.MIN_VALUE);
+System.out.println(“byte(max)=”+integer.MIN_VALUE);
+System.out.println(“byte(bit-length)=”+integer.MIN_VALUE);
+//short(16-bit signed integer)
+System.out.println(“short(min)=”+integer.MIN_VALUE);
+System.out.println(“short(max)=”+integer.MIN_VALUE);
+System.out.println(“short(bit-length)=”+integer.MIN_VALUE);
+//long(64 bit signed integer)
+System.out.println(“long(min)=”+integer.MIN_VALUE);
+System.out.println(“long(max)=”+integer.MIN_VALUE);
+System.out.println(“long(bit-length)=”+integer.MIN_VALUE);
+//char(16-bit character or 16-bit unsigned integer)
+System.out.println(“char(min)=”+integer.MIN_VALUE);
+System.out.println(“char(max)=”+integer.MIN_VALUE);
+System.out.println(“character SIZE”);
+//float(32-bit floating point)
+System.out.println(“float(min)=”+integer.MIN_VALUE);
+System.out.println(“float(max)=”+integer.MIN_VALUE);
+System.out.println(“float(bit-length)=”+integer.MIN_VALUE);
+//double(64-bit floating point)
+System.out.println(“double(min)=”+integer.MIN_VALUE);
+System.out.println(“double(max)=”+integer.MIN_VALUE);
+System.out.println(“double(bit-length)=”+integer.MIN_VALUE);
+}
+}
